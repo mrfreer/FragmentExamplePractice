cd86287fe037f2e316157716158cc3ed8b911f08
shaggy0798 <david.freer@gmail.com>
2017-09-21 15:10:09 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/fragmentexamplepractice/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.android.fragmentexamplepractice;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeFragment(View view){
+        Fragment fragment;
+        if(view == findViewById(R.id.buttonFragmentOne)){
+            fragment = new FragmentOne();
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_place, fragment);
+            ft.commit();
+        }
+        if(view == findViewById(R.id.buttonFragmentTwo)){
+            fragment = new FragmentTwo();
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_place, fragment);
+            ft.commit();
+        }
+    }
+}
